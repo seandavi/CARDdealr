a9ca60913b7b746b265e81406c14e6599c0b9657
Burus <jtburu2@uky.edu>
2023-11-02 00:20:10 -0400

    Add new functions

---
+++ R/src_epa_tri_data.R
@@ -0,0 +1,99 @@
+#' EPA Toxic Release Inventory (TRI) Dataset
+#'
+#' @description
+#' `src_epa_tri_data()` returns a dataframe of facilities reporting into the Toxic
+#' Release Inventory.
+#' 
+#' @details
+#' The Envirofacts Multisystem Search integrates information from a variety of 
+#' databases and includes latitude and longitude information. Each of these databases 
+#' contains information about facilities that are required to report activity to 
+#' a state or federal system. The TRI contains information about all facilities 
+#' releasing reportable toxic materials, identified as carcinogenic or not.
+#' 
+#' @import lgr
+#' @import dplyr
+#' @importFrom tidyr pivot_wider
+#' @importFrom stringr str_pad str_to_title str_c
+#' 
+#' @return A data frame (data.frame) containing data pulled from the Envirofacts API.
+#'
+#' @source - [Data Source](https://data.epa.gov/efservice/downloads/tri/mv_tri_basic_download/)
+#'
+#' @family data accessors
+#'
+#' @author Todd Burus <tburus@uky.edu>
+#'
+#' @examples
+#' # example code
+#' 
+#' tri = src_epa_tri_data()
+#'
+#' colnames(tri)
+#'
+#' dplyr::glimpse(tri)
+#'
+#' @export
+
+src_epa_tri_data = function(){
+    options(dplyr.summarise.inform = FALSE)
+    
+    lg <- get_carddealr_logger()
+    
+    lg$info('Starting epa_tri_data')
+    
+    yr = as.integer(format(Sys.Date(), "%Y")) -1
+    
+    url = paste0('https://data.epa.gov/efservice/downloads/tri/mv_tri_basic_download/',
+                 yr, '_US/csv')
+    
+    tri = try(read.csv(url, header=T), silent = TRUE)
+    
+    if (class(tri) == 'try-error'){
+        url = paste0('https://data.epa.gov/efservice/downloads/tri/mv_tri_basic_download/',
+                     yr-1, '_US/csv')
+        
+        tri = read.csv(url, header=T)
+    }
+    
+    colnames(tri) = str_split_fixed(colnames(tri), pattern = '\\.\\.', 2)[,2]
+    
+    tri = tri |>
+        dplyr::filter(!(ST %in% c('AS', 'GU', 'MP', 'PR', 'VI'))) |>
+        dplyr::select(FRS.ID, FACILITY.NAME, STREET.ADDRESS, CITY, ST, ZIP, COUNTY,
+                      LATITUDE, LONGITUDE, CHEMICAL, CARCINOGEN) |>
+        dplyr::mutate(
+            FACILITY.NAME = stringr::str_to_title(FACILITY.NAME),
+            ZIP = stringr::str_pad(ZIP, side='left', width=5, pad=0),
+            Address = paste0(stringr::str_to_title(STREET.ADDRESS), ', ', 
+                             stringr::str_to_title(CITY), ', ', 
+                             ST, ' ', ZIP),
+            Type = 'Toxic Release Inventory Facility'
+        ) |>
+        dplyr::rename(
+            Name = FACILITY.NAME,
+            State = ST,
+            latitude = LATITUDE,
+            longitude = LONGITUDE
+        ) |>
+        dplyr::group_by(FRS.ID, Type, Name, Address, State, latitude, longitude, CARCINOGEN) |>
+        dplyr::summarise(chemicals = list(unique(CHEMICAL))) |>
+        tidyr::pivot_wider(names_from = CARCINOGEN, values_from = chemicals) |>
+        dplyr::ungroup() |>
+        dplyr::rename(
+            Noncarcinogenic = NO,
+            Carcinogenic = YES
+        ) |>
+        dplyr::rowwise() |>
+        dplyr::mutate(
+            Noncarcinogenic = paste0(stringr::str_c(Noncarcinogenic, collapse = ', ')),
+            Carcinogenic = paste0(stringr::str_c(Carcinogenic, collapse = ', ')),
+            Phone_number = ''
+        ) |>
+        dplyr::ungroup() |>
+        dplyr::select(Type, Name, Address, State, Phone_number, Noncarcinogenic, Carcinogenic, latitude, longitude)
+    
+    lg$info('Completing epa_tri_data')
+    
+    return(tri)
+}
---
+++ R/src_fda_mammography_data.R
@@ -0,0 +1,87 @@
+#' FDA Certified Mammography Facilities Dataset
+#'
+#' @description
+#' `src_fda_mammography_data()` returns a dataframe of certified mammography facilities
+#' from the FDA Certified Mammography Facilities dataset.
+#' 
+#' @details
+#' The Mammography Quality Standards Act requires mammography facilities across the nation 
+#' to meet uniform quality standards. Congress passed this law in 1992 to assure high-quality 
+#' mammography for early breast cancer detection, which can lead to early treatment, a range of 
+#' treatment options, and increased chances of survival. Under the law, all mammography facilities 
+#' must: 1) be accredited by an FDA-approved accreditation body, 2) be certified by FDA, 
+#' or its State, as meeting the standards, 3) undergo an annual MQSA inspection, 
+#' and 4) prominently display the certificate issued by the agency. All facilities meeting this criteria
+#' are listed in the FDA Certified Mammography Facilities dataset
+#' 
+#' @import lgr
+#' @importFrom dplyr mutate select filter
+#' 
+#' @return A data frame (data.frame) containing data pulled from the Envirofacts API.
+#'
+#' @source - [Data Source](https://www.fda.gov/radiation-emitting-products/consumer-information-mqsa/search-certified-facility)
+#'
+#' @family data accessors
+#'
+#' @author Todd Burus <tburus@uky.edu>
+#'
+#' @examples
+#' # example code
+#' 
+#' mam = src_fda_mammography_data()
+#'
+#' colnames(mam)
+#'
+#' dplyr::glimpse(mam)
+#'
+#' @export
+
+src_fda_mammography_data = function(){
+    lg <- get_carddealr_logger()
+    
+    lg$info('Starting fda_mammography_data')
+    
+    url = "http://www.accessdata.fda.gov/premarket/ftparea/public.zip"
+    
+    #create tempfile to store zip file in
+    temp = tempfile()
+    
+    #download zip file
+    download.file(url, temp)
+    
+    #read files inside zip file
+    files = unzip(temp, list = TRUE)
+    
+    #locate files inside zip file
+    txt = files$Name[str_detect(files$Name, pattern = '.txt')]
+    
+    #read file to dataframe
+    df = read.csv(unz(temp, txt), sep = '|', header=F)
+    
+    #unlink the temp file
+    unlink(temp)
+    
+    #change column names
+    colnames(df) = c('Name','Street','Street2','Street3','City','State','Zip_code','Phone_number', 'Fax')
+    
+    #manipulate dataframe
+    df = df |> 
+        dplyr::filter(!(State %in% c('AP', 'AE', 'GU', 'VI'))) |> 
+        dplyr::mutate(
+            BestStreet = dplyr::case_when(
+                str_detect(Street, 'dba') ~ Street2,
+                str_detect(Street, '[0-9]|[One]', negate = T) ~ Street2,
+                .default = Street
+                ),
+            Type = 'Mammography',
+            Notes = '',
+            Address = paste0(BestStreet, ', ', City, ', ', State, ' ', Zip_code),
+            latitude = '',
+            longitude = ''
+            ) |> 
+        dplyr::select('Type','Name','Address','State', 'Phone_number', 'Notes', 'latitude', 'longitude')
+    
+    lg$info('Completing fda_mammography_data')
+    
+    return(df)
+}
