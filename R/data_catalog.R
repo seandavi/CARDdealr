53d86cb0c0d3f24f293b1d53703a72a6c8a394c5
Sean Davis <seandavi@gmail.com>
2023-11-09 07:49:32 -0500

    Add dataset_details functionality

---
+++ R/data_catalog.R
@@ -0,0 +1,134 @@
+.catalog_file=system.file('data_catalog/catalog.csv',package='CARDdealr')
+.dataset_details_file = system.file('data_catalog/dataset_details.yaml',
+                                    package='CARDdealr')
+#.dataset_details_file = 'abc.yaml'
+#' list and detail available CARDdealr datasets 
+#'
+#' @importFrom jsonlite fromJSON
+#' @importFrom tibble as_tibble
+#'
+#' @return
+#' A data.frame in which each row represents an available dataset.
+#'
+#' - name: the short name of the dataset
+#' - accessor: accessor function
+#'
+#' @examples
+#' res = available_datasets()
+#' res
+#' # and how to use the accessor programmatically
+#' get(res[1,]$accessor)()
+#'
+#' 
+#' 
+#'
+#' @export
+available_datasets <- function() {
+  catalog = .catalog_file
+  res = readr::read_csv(catalog,col_types = readr::cols())
+  class(res) = c('cdlr_avail_datasets',class(res))
+  res
+}
+
+
+
+#' @describeIn available_datasets
+#' 
+#' @details 
+#' `dataset_details`: returns a pre-computed set of column names and types,
+#'   dimensions of the datasets, and for datasets with a date (time course),
+#'   the min and max dates included in the dataset. Each dataset is an item
+#'   in the list. See examples for details and for viewing suggestions.
+#' 
+#' @return a list, with each dataset as an item
+#' 
+#' @examples 
+#' dd = dataset_details()
+#' str(dd,list.len=3)
+#' names(dd$datasets)
+#' # evaluated
+#' dd$eval_date
+#' 
+#' 
+#' @export
+dataset_details = function() {
+  if(!file.exists(.dataset_details_file)) {
+    return(list(datasets=list(), eval_date=as.character(Sys.Date())))
+  }
+  yaml::yaml.load_file(.dataset_details_file)
+}
+
+write_dataset_details = function(dd) {
+  dd[['eval_date']] = as.character(Sys.Date())
+  writeLines(yaml::as.yaml(dd),'inst/data_catalog/dataset_details.yaml')
+}
+
+add_or_update_dataset_details = function(accessor_name,...) {
+  res = dataset_details()
+  res$datasets[[accessor_name]]=create_dataset_detail_record(accessor_name,...)
+  write_dataset_details(res)
+}
+
+create_dataset_detail_record = function(accessor_name,...) {
+  a = get(accessor_name)(...)
+  ret = list(columns=lapply(a, class), 
+             dimensions=list(nrow=nrow(a), ncol = ncol(a)))
+  ret
+}
+
+#' create data details yaml file for all datasets
+#' 
+#' This function runs available_datasets() and then
+#' loops over the datasets to collect a list that looks like:
+#' 
+#' ```
+#' kff_icu_beds:
+#' columns:
+#'     fips: character
+#' county: character
+#' st: character
+#' state: character
+#' hospitals_in_cost_reports: numeric
+#' Hospitals_in_HC: numeric
+#' all_icu: numeric
+#' Total_pop: numeric
+#' 60plus: numeric
+#' 60plus_pct: numeric
+#' 60plus_per_each_icu_bed: numeric
+#' dimensions:
+#'     nrow: 3142
+#' ncol: 11
+#' us_county_geo_details:
+#'     columns:
+#'     state: character
+#' fips: character
+#' ansicode: character
+#' county: character
+#' area_land: numeric
+#' area_water: numeric
+#' area_land_sqmi: numeric
+#' area_water_sqmi: numeric
+#' geometry:
+#'     - sfc_POINT
+#'     - sfc
+#' dimensions:
+#'     nrow: 3220
+#'     ncol: 9
+#' ```
+#' 
+#' @return No value
+#' 
+#' @importFrom yaml as.yaml
+#' 
+#' @keywords internal
+create_dataset_details = function() {
+  fname = .dataset_details_file
+  z = available_datasets()
+  res = lapply(unique(z$accessor), add_or_update_dataset_details)
+}
+
+
+flatten_data_frame = function(df) {
+  .flatten = function(x) {y = unlist(x); if(length(x)==length(y)) return(y) else return(x)}
+  as_tibble(lapply(df, .flatten))
+}
